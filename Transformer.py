2d31da336db6e3d9424c351760e3be793e875fb9
skaczylo <acskaaczylo@gmail.com>
2026-03-09 20:21:05 +0100

    Arquitectura Encoder Finalizada

--- Transformer.py
+++ Transformer.py
@@ -1,45 +1,138 @@
-import tiktoken
+"""
+ARQUITECTURA TRANSFORMER.
+En este archivo se pretende construir el modelo o la arquitectura del Transformer
+usando Encoder-Decoder representado en el paper "Attention is all you need"
+
+"""
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import torch.nn as nn
+from torch.nn import functional as F
 
 
 CONTEXT_LENGTH = 300
 D_EMBEDDING = 512
 ATTENTION_HEADS = 8
-
-gpt2 = tiktoken.get_encoding("gpt2")
-dataset_json = "dataset.json" #Ruta al json con el dataset
-
-encoder = tiktoken.Encoding(
-    name = "encoder",
-    pat_str = gpt2._pat_str,
-    mergeable_ranks = gpt2._mergeable_ranks,
-    special_tokens = {
-        **gpt2._special_tokens,
-        "<START>": len(gpt2._mergeable_ranks) +1,
-        "<END>": len(gpt2._mergeable_ranks) +2,
-        "<PAD>": len(gpt2._mergeable_ranks) +3
-    }
-)
+DROPOUT = 0.0
+NUMBER_ENCODERS = 6
+NUMBER_DECODERS= 6
 
 
 
+class AttentionHead(nn.Module):
+    """Una sola capa de atencion"""
+
+    def __init__(self, dimension):
+        super().__init__()
+        self.key = nn.Linear(in_features=D_EMBEDDING,out_features=dimension, bias=False)
+        self.query = nn.Linear(in_features=D_EMBEDDING,out_features=dimension, bias=False)
+        self.value = nn.Linear(in_features=D_EMBEDDING,out_features=dimension, bias = False)
+
+        #self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self,x,mask = None):
+
+        #B = numero de Batch; T = numero de "tokens"; C = dimension de cada token
+        B, N, D_i = x.shape
+
+        K = self.key(x) # (B, N , D_i)
+        Q = self.key(x) #(B, N , D_i)
+        V = self.key(x) #(B, N , D_i)
+
+        #Calculamos la Atención QxK^T
+        """
+        Calculamos la "afinidad" Q x K^t
+        Como tenemos batches, usamos el operador @ que aplica la multiplicacion en las dos ultimas dimensiones B veces
+        K.transpose significa transponer la penultima dimensino T con la pultima dimension C. 
+        """
+        scores = Q @ K.transpose(-2,-1) #(B, N , D_i) x (B,D_i,N) = (B,N,N)
+        scores = scores /(D_i ** 0.5)
+
+        if mask is not None:
+            scores = scores + mask  #Sumamos la mascara para evitar que se fije en tokens <PAD>
+
+        scores = F.softmax(scores, dim=-1) #Aplicamos softmax sobre las filas, es decir, sobre la ultima dimension 
+
+        attention = scores @ V
+
+        return attention
+    
+
+class MultiHeadAttention(nn.Module):
+    """Multiples capas de atencion"""
+
+    def __init__(self, num_heads):
+        super().__init__()
+
+        """
+        Block attention es la concatenacion de las capas de atencion
+        nn.ModuleList permite tener una lista de modulos y que Pytorch sea "consciente" de que existen; si haces una lista [] normal, no los reconocería a la hora de entrenar
+        """
+        self.block_attention = nn.ModuleList([AttentionHead(D_EMBEDDING // num_heads) for _ in range(num_heads)])
+        self.projection = nn.Linear(in_features=D_EMBEDDING,out_features=D_EMBEDDING,bias=False)  #Matriz Omega_O 
+
+        self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self,x,mask = None):
+        B, N, D_i = x.shape
+
+        outputs = [head(x,mask) for head in self.block_attention] #Lista de matrices Nx(D_Embedding / num_heads)
+
+        outputs = torch.cat(outputs, dim =-1) # Matriz N x D_embedding (por cada batch) => (B, N, D_embedding)
+        multiple_attention = self.projection(outputs)
+
+        return multiple_attention
+    
+
+class MLP(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features=D_EMBEDDING, out_features= 4*D_EMBEDDING),
+            nn.ReLU(),
+            nn.Linear(4*D_EMBEDDING, D_EMBEDDING),
+            nn.Dropout(DROPOUT)
+        )
+
+    def forward(self,x):
+        return self.mlp(x)
 
 
 
+class EncoderBlock(nn.Module):
+
+    """
+    Bloque que se apilará para formar el Encoder
+
+    """
+
+    def __init__(self):
+        super().__init__()
+    
+        self.attention_heads = MultiHeadAttention(num_heads= ATTENTION_HEADS)
+        self.mlp = MLP()
+        self.LayerNorm = nn.LayerNorm(D_EMBEDDING)
+
+    def forward(self,x,mask = None):
+        
+        # x = ( B, N, D_i)
+
+        z = self.LayerNorm(self.attention_heads(x,mask) + x)
+        output = self.LayerNorm(self.mlp(z) + z) # ( B, N, D_i)
+
+        return output
 
 
 
-"""
-ARQUITECTURA DEL TRANSFORMER
-"""
 class Encoder(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self, vocab_size,pad_id):
         super().__init__()
 
         """
@@ -48,64 +141,45 @@ class Encoder(nn.Module):
         Pytorch no multiplica matrices aquí, solo indica, dado un vector de indices de tokens, que filas de la matriz E extraer
         """
        
-        self.token_embedding_table = nn.Embedding(num_embeddings=vocab_size, embedding_dim=D_EMBEDDING, padding_idx= encoder._special_tokens["<PAD>"])
+        self.token_embedding_table = nn.Embedding(num_embeddings=vocab_size, embedding_dim=D_EMBEDDING, padding_idx= pad_id)
 
         """
         Positional Encoding: x_n = x_n + r_n -> r_ tiene dimensión D_EMBEDDING
         El objetivo es que permita determinar en que posición se encuentra cada palabra, por eso el numero de embeddings es CONTEXT_LENGT
         """
         self.position_embedding_table = nn.Embedding(num_embeddings= CONTEXT_LENGTH, embedding_dim=D_EMBEDDING)
+        self.encoder_blocks = nn.Sequential(*[EncoderBlock() for _ in range(NUMBER_ENCODERS)])
+
+
+
+        
+    def forward(self, x, mask = None):
+
+        B, num_tokens = x.shape
+        
+        #Input Embedding + Positional Embedding
+        x_embedding = self.token_embedding_table(x) #(B, N, D_i)
+        positional_encoding = self.position_embedding_table(torch.arange(num_tokens))
+        x_embedding = x_embedding + positional_encoding
+
+        #Bloques de Encoder encadenados
+        output = self.encoder_blocks(x_embedding)
+        return output
 
 
 
 
 
 
-"""  
-DATALOADER 
-"""
-class Dataset(Dataset):
-    def __init__(self, data):
-        self.data = data
-        self.X = self.data['en']
-        self.y = self.data['es']
         
 
-    def __len__(self):
-        return len(self.data)
-    
-    def adjust_length(self,text):
-        pad = [encoder._special_tokens["<PAD>"] for _ in range(CONTEXT_LENGTH-len(text))]
-        return np.concatenate((text,pad))
+
+
+
+
     
 
-    def __getitem__(self, idx):
-
-        en_text = encoder.encode(self.X.iloc[idx])
-        es_text = encoder.encode(self.y.iloc[idx])
 
 
-        #Truncar entrada en caso de que se supere dimension entrada x_n
-        if len(en_text) > CONTEXT_LENGTH:
-            en_text =  en_text[:300]
-        
-        if len(es_text) > CONTEXT_LENGTH:
-            es_text = es_text[:(CONTEXT_LENGTH-2)]
 
 
-        en_text = self.adjust_length(en_text)
-        es_text = self.adjust_length(es_text)
-
-       
-        #Añadimos tokens especiales al targe
-        es_text = np.concatenate(([encoder._special_tokens["<START>"]],
-                                  es_text[:(CONTEXT_LENGTH-2)],
-                                  [encoder._special_tokens["<END>"]]),
-                                  axis=0)
-
-
-        #COnvertimos a tensores
-        es_text =  torch.tensor(es_text,dtype=torch.long)
-        en_text = torch.tensor(en_text,dtype= torch.long)
-
-        return en_text, es_text
---
+++ dataset.py
@@ -0,0 +1,100 @@
+
+import tiktoken
+from  Transformer import CONTEXT_LENGTH
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+class Tokenizer():
+    """
+    TOKENIZADOR
+    Clase que permitirá dividir o tokenizar un string en tokens. 
+    Se usará el algoritmo ya implementado de la liberia tiktoken y el vocabulario de GPT2
+    """
+
+    def __init__(self):
+        self.gpt2 = tiktoken.get_encoding("gpt2")  #No confundir con el encoder del Transformer
+
+        self.encoder = tiktoken.Encoding(
+            name = "encoder",
+            pat_str = self.gpt2._pat_str,
+            mergeable_ranks = self.gpt2._mergeable_ranks,
+            special_tokens = {
+                **self.gpt2._special_tokens,
+                "<START>": len(self.gpt2._mergeable_ranks) +1,
+                "<END>": len(self.gpt2._mergeable_ranks) +2,
+                "<PAD>": len(self.gpt2._mergeable_ranks) +3
+            }
+        )
+
+
+    def __len__(self):
+        return len(self.encoder._mergeable_ranks) + len(self.encoder._special_tokens)
+    
+
+    def pad_id(self):
+        return self.encoder._special_tokens["<PAD>"]
+    
+    def end_of_text_id(self):
+        return self.encoder._special_tokens["<END>"]
+    
+    def star_of_text_id(self):
+        return self.encoder._special_tokens["<START>"]
+    
+    def encode(self,input):
+
+        return self.encoder.encode(input)
+
+    def decoder(self,input):
+
+        return self.encoder.decode(input)
+    
+
+
+# Dataset
+class Dataset_(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.X = self.data['en']
+        self.y = self.data['es']
+        self.tokenizer = tokenizer
+        
+
+    def __len__(self):
+        return len(self.data)
+    
+    def adjust_length(self,text):
+        pad = [self.tokenizer.pad_id() for _ in range(CONTEXT_LENGTH-len(text))]
+        return np.concatenate((text,pad))
+    
+
+    def __getitem__(self, idx):
+
+        en_text = self.tokenizer.encode(self.X.iloc[idx])
+        es_text = self.tokenizer.encode(self.y.iloc[idx])
+
+
+        #Truncar entrada en caso de que se supere dimension entrada x_n
+        if len(en_text) > CONTEXT_LENGTH:
+            en_text =  en_text[:300]
+        
+        if len(es_text) > CONTEXT_LENGTH:
+            es_text = es_text[:(CONTEXT_LENGTH-2)]
+
+
+        en_text = self.adjust_length(en_text)
+        es_text = self.adjust_length(es_text)
+
+       
+        #Añadimos tokens especiales al targe
+        es_text = np.concatenate(([self.tokenizer.star_of_text_id()],
+                                  es_text[:(CONTEXT_LENGTH-2)],
+                                  [self.tokenizer.end_of_text_id()]),
+                                  axis=0)
+
+
+        #COnvertimos a tensores
+        es_text =  torch.tensor(es_text,dtype=torch.long)
+        en_text = torch.tensor(en_text,dtype= torch.long)
+
+        return en_text, es_text
